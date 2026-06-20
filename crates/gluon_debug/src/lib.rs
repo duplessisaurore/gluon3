33c4aa072239ed621a864f296b4dbeb3165bb443
stellaurora <jean1398reborn@gmail.com>
2026-06-20 13:14:11 +1000

    feat: clippy and fmt gluon_debug

--- crates/gluon_debug/src/lib.rs
+++ crates/gluon_debug/src/lib.rs
@@ -1,4 +1,4 @@
-//! `Gluon3` is an experimental free and open-source compiler for the `Fermion3` 
+//! `Gluon3` is an experimental free and open-source compiler for the `Fermion3`
 //! language that translates `Fermion3` into the more textual assembly language `Quark3`.
 //!
 //! Check out the [repository README](https://github.com/duplessisaurore/gluon3/blob/main/README.md)
@@ -32,10 +32,12 @@ pub struct SourceLocation {
 }
 
 impl SourceLocation {
+    #[must_use]
     pub fn new(file: Rc<SourceFile>, span: Span) -> Self {
         Self { file, span }
     }
 
+    #[must_use]
     pub fn filename(&self) -> &str {
         self.file.filename.as_str()
     }
@@ -57,6 +59,7 @@ pub struct Span {
 }
 
 impl SourceLocation {
+    #[must_use]
     pub fn new_span_in_file(&self, span: Span) -> Self {
         Self {
             file: self.file.clone(),
@@ -66,6 +69,7 @@ impl SourceLocation {
 }
 
 impl Span {
+    #[must_use]
     pub fn join(self, other: Span) -> Span {
         Span {
             start: self.start,
