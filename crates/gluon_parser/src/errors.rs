955b7cd87b90ddaf4ecc83df68ce27550d3f76f0
stellaurora <jean1398reborn@gmail.com>
2026-06-21 12:20:23 +1000

    feat: setup parser functionality - ParserErrors - we now can advance thrhough the stream, expect, peek match etc. like the Lexer but Parser ig

--- crates/gluon_parser/src/errors.rs
+++ crates/gluon_parser/src/errors.rs
@@ -1,13 +1,37 @@
 //! Errors that can occur during the parsing process
 
+use gluon_debug::Located;
+use gluon_lexer::TokenKind;
+
 use crate::ast::AstNode;
 
 /// Result of one step of the parsing process, this is just a convenience
 /// over having to write Result<AstNode<FileName>, `LexError`> everywhere if the token
 /// type needs to change or something.
-pub type LexResult<FileName> = Result<AstNode<FileName>, ParseError>;
+pub type ParseResult<FileName> = Result<AstNode<FileName>, LocatedParseError<FileName>>;
+
+/// We want to keep source information with the ParseErrors so
+/// the user of the parser can nicely output them.
+pub type LocatedParseError<FileName> = Located<ParseError, FileName>;
 
 /// Errors that can occur while parsing.
 #[derive(Debug, Clone, PartialEq)]
 pub enum ParseError {
+    /// We expected a specific token, but found something else
+    UnexpectedToken {
+        expected: TokenKind,
+        found: TokenKind,
+    },
+
+    /// We expected a token, but found the End Of File!
+    UnexpectedEof,
+
+    /// There is a strict operator with parenthesis rule.
+    /// 
+    /// Mixing operators without parenthesis is not permitted
+    /// read the `Fermion3` spec at the "Function Calls" section.
+    MixedOperatorsWithoutParentheses {
+        expected: TokenKind,
+        found: TokenKind,
+    }
 }
--- crates/gluon_parser/src/parser.rs
+++ crates/gluon_parser/src/parser.rs
@@ -4,12 +4,162 @@
 //! with the abstract syntax tree `AstNode`'s representing
 //! all program bits
 
-use core::fmt::Display;
+use core::fmt::{Display, Debug as DebugTrait};
+
+use alloc::{rc::Rc, vec::Vec};
+use gluon_debug::{Located, SourceFile, SourceLocation, Span};
+use gluon_lexer::{Token, TokenKind};
+
+use crate::errors::{LocatedParseError, ParseError};
 
 /// The actual parser class itself
-pub struct Parser<'src, FileName: Display + Clone> {
+pub struct Parser<FileName: Display + Clone + PartialEq> {
+    /// The stream of tokens produced by the Lexer
+    tokens: Vec<Token<FileName>>,
+
+    /// Our current position in the token stream
+    ///
+    /// We do not use Peekable or something similar since
+    /// we may want to do backtracking or other things
+    cursor: usize,
+
+    /// The source file to track from which the tokens
+    /// came from when building the module and SourceLocations.
+    file: Rc<SourceFile<FileName>>,
 }
 
-impl<'src, FileName: Display + Clone> Parser<'src, FileName> {
-    
+impl<FileName: Display + Clone + PartialEq + DebugTrait> Parser<FileName> {
+    /// Create a new parser over `tokens` that will parse all of the
+    /// tokens into a singular `Module` for this file
+    ///
+    /// All of these tokens and the produced Module will be assumed to have come
+    /// from the `file`passed in with the same conditions as the `Lexer`
+    pub fn new(tokens: Vec<Token<FileName>>, file: Rc<SourceFile<FileName>>) -> Self {
+        Self {
+            tokens,
+            cursor: 0,
+            file,
+        }
+    }
+
+    /// Look at the current token without consuming it.
+    pub fn peek_token(&self) -> Option<&Token<FileName>> {
+        self.tokens.get(self.cursor)
+    }
+
+    /// Look ahead at the `offset` token in the token stream without consuming it.
+    pub fn peek_token_nth(&self, offset: usize) -> Option<&Token<FileName>> {
+        self.tokens.get(self.cursor + offset)
+    }
+
+    /// Check if we have reached the end of the token stream.
+    pub fn is_at_end(&self) -> bool {
+        self.peek_token()
+            .map(|token| matches!(token.kind, TokenKind::Eof))
+            // If for some reason we don't have an EoF marker (lexer kaboomy?) then we have
+            // also hit the EoF i suppose.
+            .unwrap_or(true)
+    }
+
+    /// Consume the current token and advance the cursor.
+    /// Returns an UnexpectedEof error if we try to advance past the end.
+    pub fn advance(&mut self) -> Result<Token<FileName>, LocatedParseError<FileName>> {
+        if self.is_at_end() || self.cursor >= self.tokens.len() {
+            return Err(self.unexpected_eof());
+        }
+
+        let token = self.tokens[self.cursor].clone();
+        self.cursor += 1;
+        Ok(token)
+    }
+
+    /// Check if the current token matches a specific kind without consuming it.
+    pub fn check(&self, kind: &TokenKind) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+
+        self.peek_token()
+            .map(|token| &token.kind == kind)
+            .unwrap_or(false)
+    }
+
+    /// Consume the current token if it matches `expected_kind`
+    /// Otherwise, throw a `ParseError`
+    pub fn expect(
+        &mut self,
+        expected_kind: TokenKind,
+    ) -> Result<Token<FileName>, LocatedParseError<FileName>> {
+        // Matches token.. consume and advance
+        if self.check(&expected_kind) {
+            self.advance()
+        } else {
+            // Doesnt match! return what we got with the error
+            let found = self.peek_token().map(|token| token);
+
+            // If there's no token then we've hit an unexpected EoF
+            let Some(found_token) = found else {
+                return Err(self.unexpected_eof());
+            };
+
+            // There's a token that we didnt expect.
+            Err(self.make_located(
+                ParseError::UnexpectedToken {
+                    expected: expected_kind,
+                    found: found_token.kind.clone(),
+                },
+                found_token.location.span,
+            ))
+        }
+    }
+
+    /// Conditionally advance if the current token matches `kind`.
+    /// 
+    /// Returns Some(token) if it was consumed, None otherwise.
+    pub fn match_token(&mut self, kind: TokenKind) -> Option<Token<FileName>> {
+        if self.check(&kind) {
+            Some(self.advance().expect("check already checked for a token to exist here"))
+        } else {
+            None
+        }
+    }
+
+    /// Gets the span of the token we just consumed.
+    pub fn previous_span(&self) -> Span {
+        // At the start.. no token possible
+        if self.cursor == 0 {
+            Span { start: 0, end: 0 }
+        } else {
+            self.tokens[self.cursor - 1].location.span
+        }
+    }
+
+    /// Gets the span of the current token we are looking at.
+    pub fn current_span(&self) -> Span {
+        self.peek_token()
+            .map(|t| t.location.span)
+            .unwrap_or_else(|| self.previous_span())
+    }
+
+    /// Returns a new Located<T> for the kind with a source span in the current
+    /// stored file of the `Lexer`.
+    fn make_located<T: Clone + PartialEq>(
+        &self,
+        kind: T,
+        source_span: Span,
+    ) -> Located<T, FileName> {
+        Located {
+            kind,
+            location: SourceLocation {
+                file: Rc::clone(&self.file),
+                span: source_span,
+            },
+        }
+    }
+
+    /// Returns an `UnexpectedEof` `ParseError` with the source location of
+    /// the previous `Token`'s span
+    fn unexpected_eof(&self) -> LocatedParseError<FileName> {
+        self.make_located(ParseError::UnexpectedEof, self.previous_span())
+    }
 }
